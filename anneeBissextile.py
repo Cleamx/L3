9e79d12b0a767d0e46132ed0bcac5dd56a0c6d9b
clem <clementine.mirande@gmail.com>
2023-09-07 13:54:05 +0200

    exerciceDeux

---
+++ anneeBissextile.py
@@ -0,0 +1,42 @@
+import random
+
+def testAnnee():
+    i = 0
+
+    while i != 10:
+        anneeTest = random.randint(1000,3000)
+        est_bessextile(anneeTest)
+        print(anneeTest,anneeBool)
+        i+=1
+
+def est_bessextile(annee:int):
+    global anneeBool
+
+    if annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0:
+        anneeBool = True
+    else:
+        anneeBool = False
+
+    return(anneeBool)
+
+def main():
+    
+    testValeur = True
+    while testValeur == True:
+        entrerAnnee = input("Entrer une année : ")
+        try:
+            entrerAnnee = int(entrerAnnee)
+            int(entrerAnnee) == int()
+            testValeur = False
+        except ValueError:
+            print("Désolé la valeur saisie n'est pas correct.")
+    
+    est_bessextile(entrerAnnee)
+
+    if anneeBool == True:
+        print(entrerAnnee," est une année bissextile")
+    elif anneeBool == False:
+         print(entrerAnnee, " n'est pas bissextile.")
+        
+testAnnee()
+main()
