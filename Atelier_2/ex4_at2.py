95c00d3273e6ae0a476c9bfbe168dcdb30a4574f
clem <clementine.mirande@gmail.com>
2023-09-11 16:42:00 +0200

    debut exercice 4

---
+++ Atelier_2/ex4_at2.py
@@ -0,0 +1,82 @@
+def histo(F: list) -> list:
+    """liste d'entiers H représentant l'histogramme de F
+
+    Args:
+        F (list): liste d'entier
+
+    Returns:
+        list: liste d'entiers H représentant l'histogramme de F
+    """
+    MAX_VALEUR = max(F)
+
+    H = [0] * (MAX_VALEUR + 1)
+
+    for i in F:
+        H[i] += 1
+    return H
+
+def est_injective(F: list)->bool:
+    """retourne True ou False si la liste F est une injection
+
+    Args:
+        F (list): liste d'entier
+
+    Returns:
+        bool: True si la liste F est une injection, sinon False
+    """
+    H = histo(F)
+    injective = False
+
+    if all(valeur <= 1 for valeur in H):
+        injective = True
+    return injective
+
+def est_surjective(F:list) -> list:
+    """retourne True ou False si la liste F est surjective
+
+    Args:
+        F (list): liste d'entier
+
+    Returns:
+        list: True ou Flase si la liste est surjective ou non
+    """
+    H = histo(F)
+    surjective = False
+
+    if all(valeur >= 1 for valeur in H):
+        surjective = True
+    return surjective
+
+def est_bijective(F:list) -> bool:
+    """retourne True ou False si la liste F est bijective
+
+    Args:
+        F (list): liste d'entier
+
+    Returns:
+        bool: retourne True ou False si la liste F est surjective
+    """
+    bijective = False
+
+    if est_bijective(F) and est_injective(F) == True:
+        bijective = True
+    return bijective
+
+def AffichageHisto(F:list)->str:
+    H = histo(F)
+    MAXOCC = max(H)
+
+    for i in range(MAXOCC, 0, -1):
+        line = ''
+        for j in range(len(H)):
+            valeur = H[j]
+            if valeur >= i:
+                line += " #|"
+            elif valeur <= i:
+                line += "  |"
+        print(line)
+
+    lignes = '  '.join([str(num) for num in range(len(H))])
+    print(lignes)
+
+print(AffichageHisto([1,1,2,4,5,6,6,4,6,7,8,9,9,10,11,11,12,13,14,15]))
