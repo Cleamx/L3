a85cdce2276517477ff3432f5ab96835c55775e2
clem <clementine.mirande@gmail.com>
2023-09-12 13:46:48 +0200

    vitrines

--- Atelier_2/ex5_at2.py
+++ Atelier_2/ex5_at2.py
@@ -4,12 +4,12 @@ def vitrines(nbEmplacements:int, lObjet:list)->list:
     i = 0
 
     for i in range(len(lObjet)):
-        if len(vitrineUn) < nbEmplacements and lObjet[i] not in vitrineUn :
+        if len(vitrineUn) <= len(vitrineDeux) and len(vitrineUn) < nbEmplacements and lObjet[i] not in vitrineUn :
             vitrineUn.append(lObjet[i])
         elif len(vitrineDeux) < nbEmplacements and lObjet[i] not in vitrineDeux:
             vitrineDeux.append(lObjet[i])
-    
+
     print(vitrineUn, vitrineDeux)
 
-lObjets = [1, 2, 2, 3, 4, 5, 5]
-print(vitrines(4, lObjets))
+lObjets = [1, 2, 2, 2, 3, 4, 5, 5, 4, 6]
+print(vitrines(5, lObjets))
