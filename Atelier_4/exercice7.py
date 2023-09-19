4e65ca3bcf0e7629e719cb2d68836b2f41ad3326
clem <clementine.mirande@gmail.com>
2023-09-19 15:47:49 +0200

    add docstring and question 3

--- Atelier_4/exercice7.py
+++ Atelier_4/exercice7.py
@@ -22,13 +22,14 @@ def stupid_sort(lst_to_sort:list)->list:
 print(stupid_sort([1,2]))
 
 def insertion_sort(list_to_sort: list)->list:
-    """fonction qui 
+    """fonction qui trie la liste en décalant vers la droite les éléments
+    qui sont plus grands que x
 
     Args:
-        list_to_sort (list): _description_
+        list_to_sort (list): liste à trier
 
     Returns:
-        list: _description_
+        list: liste triée
     """
     list_to_sort1 = list(list_to_sort)
     
@@ -44,3 +45,26 @@ def insertion_sort(list_to_sort: list)->list:
 
 my_lst_to_sort = [170, 45, 75, 90, 2, 24, 802, 66]
 print(insertion_sort(my_lst_to_sort))
+
+def selection_sort(list_to_sort: list)->list:
+    """fonction qui recherche dans la partie de la liste non triée le min
+    et place le min à la fin de la partie triée de la liste 
+
+    Args:
+        list_to_sort (list): liste à trier
+
+    Returns:
+        list: liste triée ordre croissant
+    """
+
+    for i in range(len(list_to_sort)):
+        min_index = i
+        for j in range(i+1, len(list_to_sort)):
+            if list_to_sort[j] < list_to_sort[min_index]:
+                min_index = j
+        if min_index != i:
+            list_to_sort[i], list_to_sort[min_index] = list_to_sort[min_index], list_to_sort[i]
+    
+    return list_to_sort
+
+print(selection_sort(my_lst_to_sort))
