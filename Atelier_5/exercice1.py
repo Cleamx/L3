3f4bd6730a7d4f0df02346b41b793a234dec04a7
clem <clementine.mirande@gmail.com>
2023-09-21 15:04:07 +0200

    exercice1

--- Atelier_5/exercice1.py
+++ Atelier_5/exercice1.py
@@ -1,6 +1,58 @@
 import numpy as np
 
 
-arr = np.array([6, 7, 8, 9]) 
-x = np.searchsorted(arr, 7)
-print(x)
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
+print(arr)
+
+def my_searchsorted (table : object, indice : int)-> int:
+    """fonction qui cherche l'indice d'un élément d'une matrice
+
+    Args:
+        table (object): table d'élément
+        indice (int): indice recherchée
+
+    Returns:
+        int: indice de l'élément rechercher
+    """
+    for i in table:
+        if indice == table[i]:
+            return i
+
+print(my_searchsorted(arr,4))
+
+def my_where(table : object, indice : int )-> list:
+    """fonction qui cherche l'indice d'un élément d'une matrice et retourne une liste d'un ou plusieurs indices
+
+    Args:
+        table (object): table d'élément
+        indice (int): indice rechercher
+
+    Returns:
+        list: liste d'un ou plusieurs élément 
+    """
+    res = []
+    for i in range(len(table)):
+        if indice == table[i]:
+            res.append(i)
+    return res 
+
+print(my_where(arr, 4))
+
+A = np.array(([3,1,],[6,4])) 
+B = np.array(([1,8],[4,2]))
+
+def my_add(tableA : object, tableB : object)-> object:
+    """fonction qui permet d'additionner deux matrice
+
+    Args:
+        tableA (object): matrice a
+        tableB (object): matrice b
+
+    Returns:
+        object: matrice a+b
+    """
+    if tableA.shape == tableB.shape:
+        res = A + B
+    return res
+
+print(my_add(A,B))
