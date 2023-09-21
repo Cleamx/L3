fefcd31b005fbad91968f6bcadc22bd9c8203677
clem <clementine.mirande@gmail.com>
2023-09-21 11:13:42 +0200

    recursive

--- Atelier_5/recursive.py
+++ Atelier_5/recursive.py
@@ -1,20 +1,23 @@
-def somme_recursive(liste:list)->list:
+lst_test = [1,2,3,4]
+
+
+def somme_recursive(liste:list)->int:
     """fonction qui calcul la somme d'une liste grâce à la récursivité
 
     Args:
         liste (list): liste
 
     Returns:
-        list: somme
+        int: somme
     """
     
-    if len(liste) == 1:
-        return liste[0]
+    if liste == []:
+        return 0
     else:
         return liste[0] + somme_recursive(liste[1:])
     
 
-print(somme_recursive([1,2,3,4]))
+print(somme_recursive(lst_test))
 
 def factorielle_recursive(nombre:int) -> int:
     """fonction qui calcul la factorielle de nombre grâce à la récursivité
@@ -32,3 +35,52 @@ def factorielle_recursive(nombre:int) -> int:
         return factorielle_recursive(nombre-1)*nombre
 
 print(factorielle_recursive(5))
+
+def longueur(lst:list)->int:
+    """fonciton qui retourne la taille de la liste
+
+    Args:
+        lst (list): liste
+
+    Returns:
+        int: taille de la liste
+    """
+    if lst == []:
+        return 0
+    else:
+        return 1 + longueur(lst[1:])
+
+print(longueur(lst_test))
+
+def findMin(lst:list)->int:
+    minNumber = 0
+    if len(lst) == 1:
+        minNumber =  lst[0]
+    elif len(lst) == 0:
+        minNumber = None
+    else:
+        
+        if lst[0] < minNumber:
+            minNumber = lst[0]
+        else:
+            minNumber = findMin(lst[1:])
+    return minNumber
+
+print(findMin(lst_test))
+
+def ListPairs(lst : list)->list:
+    if len(lst) == 0:
+        return []
+    elif len(lst) == 1:
+        if lst[0] % 2 == 0:
+            return lst
+        else:
+            return []
+    else:
+        lst1 = ListPairs(lst[1:])
+        if lst[0] % 2 == 0:
+            return [lst[0]] + lst1
+        else:
+            return lst1
+
+print(ListPairs(lst_test))
