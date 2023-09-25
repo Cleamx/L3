2abbf9e28ab8a9171634c1c388d889fc39c64c51
clem <clementine.mirande@gmail.com>
2023-09-25 11:05:08 +0200

    modif docstring

--- Atelier_5/exercice1.py
+++ Atelier_5/exercice1.py
@@ -4,39 +4,39 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 4, 4])
 print(arr)
 
-def my_searchsorted (table : object, indice : int)-> int:
+def my_searchsorted (table : object, element : int)-> int:
     """fonction qui cherche l'indice d'un élément d'une matrice
 
     Args:
         table (object): table d'élément
-        indice (int): indice recherchée
+        indice (int): element recherchée
 
     Returns:
         int: indice de l'élément rechercher
     """
     for i in table:
-        if indice == table[i]:
+        if element == table[i]:
             return i
 
 print(my_searchsorted(arr,4))
 
-def my_where(table : object, indice : int )-> list:
+def my_where(table : object, element : int )-> list:
     """fonction qui cherche l'indice d'un élément d'une matrice et retourne une liste d'un ou plusieurs indices
 
     Args:
         table (object): table d'élément
-        indice (int): indice rechercher
+        indice (int): element rechercher
 
     Returns:
         list: liste d'un ou plusieurs élément 
     """
     res = []
     for i in range(len(table)):
-        if indice == table[i]:
+        if element == table[i]:
             res.append(i)
     return res 
 
-print(my_where(arr, 4))
+# print(my_where(arr, 4))
 
 A = np.array(([3,1,],[6,4])) 
 B = np.array(([1,8],[4,2]))
@@ -55,4 +55,4 @@ def my_add(tableA : object, tableB : object)-> object:
         res = A + B
     return res
 
-print(my_add(A,B))
+# print(my_add(A,B))
