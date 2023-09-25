14d1baa6b063a7b8bf7aac3c50304224c227c553
clem <clementine.mirande@gmail.com>
2023-09-25 15:04:57 +0200

    add test()

--- Atelier_5/recursive.py
+++ Atelier_5/recursive.py
@@ -1,5 +1,3 @@
-lst_test = [1,2,3,4]
-
 
 def somme_recursive(liste:list)->int:
     """fonction qui calcul la somme d'une liste grâce à la récursivité
@@ -15,9 +13,7 @@ def somme_recursive(liste:list)->int:
         return 0
     else:
         return liste[0] + somme_recursive(liste[1:])
-    
 
-print(somme_recursive(lst_test))
 
 def factorielle_recursive(nombre:int) -> int:
     """fonction qui calcul la factorielle de nombre grâce à la récursivité
@@ -34,8 +30,6 @@ def factorielle_recursive(nombre:int) -> int:
     else:
         return factorielle_recursive(nombre-1)*nombre
 
-print(factorielle_recursive(5))
-
 def longueur(lst:list)->int:
     """fonciton qui retourne la taille de la liste
 
@@ -49,8 +43,6 @@ def longueur(lst:list)->int:
         return 0
     else:
         return 1 + longueur(lst[1:])
-
-print(longueur(lst_test))
 
 def findMin(lst:list)->int:
     """fonction qui retourne le minimum d'une liste en utilisant la recursivité
@@ -73,8 +65,6 @@ def findMin(lst:list)->int:
         else:
             minNumber = findMin(lst[1:])
     return minNumber
-
-print(findMin(lst_test))
 
 def ListPairs(lst : list)->list:
     """fonction qui retourne les éléments paires dans une liste en utilisant la recursivité
@@ -100,8 +90,6 @@ def ListPairs(lst : list)->list:
         else:
             return paires
 
-print(ListPairs(lst_test))
-
 def concat_lst(lst : list)->list:
     """fonction qui permet de concaténer plusieurs listes
 
@@ -115,8 +103,6 @@ def concat_lst(lst : list)->list:
         return []
     else:
         return lst[0] + concat_lst(lst[1:])
-    
-print(concat_lst([[0,1],[2,3],[4],[5,6]]))
 
 def separe(lst)->tuple:
     """fonction qui sépare une liste en deux et qui renvoie un tuple de liste avec une liste paire et une impaire
@@ -137,4 +123,48 @@ def separe(lst)->tuple:
     else:
         return pairs, [lst[0]] + impairs
 
-print(separe(lst_test))
+
+def test():
+    lst_test = [1, 2, 3, 4]
+
+    # Test de somme_recursive
+    print("Test de somme_recursive:")
+    print("Somme de", lst_test, ":", somme_recursive(lst_test))
+    print("---------------------------")
+
+    # Test de factorielle_recursive
+    n = 5
+    print("Test de factorielle_recursive:")
+    print(f"Factorielle de {n} :", factorielle_recursive(n))
+    print("---------------------------")
+
+    # Test de longueur
+    print("Test de longueur:")
+    print("Longueur de", lst_test, ":", longueur(lst_test))
+    print("---------------------------")
+
+    # Test de findMin
+    print("Test de findMin:")
+    print("Minimum de", lst_test, ":", findMin(lst_test))
+    print("---------------------------")
+
+    # Test de ListPairs
+    print("Test de ListPairs:")
+    print("Éléments pairs de", lst_test, ":", ListPairs(lst_test))
+    print("---------------------------")
+
+    # Test de concat_lst
+    lst_of_lists = [[0, 1], [2, 3], [4], [5, 6]]
+    print("Test de concat_lst:")
+    print("Concaténation de listes :", concat_lst(lst_of_lists))
+    print("---------------------------")
+
+    # Test de separe
+    print("Test de separe:")
+    paire, impaire = separe(lst_test)
+    print("Liste des éléments pairs :", paire)
+    print("Liste des éléments impairs :", impaire)
+    print("---------------------------")
+
+# Appeler la fonction de test
+test()
