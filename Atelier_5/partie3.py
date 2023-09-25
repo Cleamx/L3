f0fd84dc4a3175441a767d6c43ccec0aafc55749
clem <clementine.mirande@gmail.com>
2023-09-25 13:50:06 +0200

    q3

--- Atelier_5/partie3.py
+++ Atelier_5/partie3.py
@@ -23,7 +23,7 @@ def matriceAdjacence(S: list, A: list)->object:
 lst_sommet = [0,1,2,3,4]
 lst_arc = [(0,1),(0,2),(1,2), (1,4), (2,3), (3,4), (4,2)]
 res = matriceAdjacence(lst_sommet, lst_arc)
-print(res)
+# print(res)
 
 def matriceAdjacencePond(S: list, A: list)->object:
     """fonction qui retourne la matrice d’adjacence 
@@ -46,4 +46,32 @@ def matriceAdjacencePond(S: list, A: list)->object:
 
 lst_arc_pond = [(0,1,3),(0,2,5),(1,2,4), (1,4,1), (2,3,9), (3,4,8), (4,2,7)]
 resPond = matriceAdjacencePond(lst_sommet, lst_arc_pond)
-print(resPond)
+# print(resPond)
+
+def lireMatriceFichier(nomfichier)->object:
+    """fonction qui lit dans un fichier une matrice et qui crée une matrice avec numpy à partir du fichier
+
+    Args:
+        nomfichier (file): nom du fichier
+
+    Returns:
+        object: matrice
+    """
+    try:
+        with open(nomfichier, 'r') as fichier:
+            lignes = fichier.readlines()
+        
+        lignes_matrice = [ligne.strip().split() for ligne in lignes]
+        matrice_np = np.array([list(map(float, ligne)) for ligne in lignes_matrice])
+        
+        if matrice_np.shape[0] == matrice_np.shape[1]:
+            return matrice_np
+        else:
+            raise ValueError("La matrice n'est pas carrée.")
+    
+    except FileNotFoundError:
+        raise FileNotFoundError("Le fichier n'existe pas.")
+
+nomFick = "./Atelier_5/graphe/graph3.txt"
+
+print(lireMatriceFichier(nomFick))
