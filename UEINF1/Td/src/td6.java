2ac9539a35c1ee2a336883eea8a9da03b9e4ba36
clem <clementine.mirande@gmail.com>
2023-10-11 10:31:57 +0200

    "modif"

---
+++ UEINF1/Td/src/td6.java
@@ -0,0 +1,11 @@
+package Td1.Td.src;
+
+public class td6 {
+    public static void main(String[] args) {
+        char ch = '5';
+        int ascii = ch;
+
+        System.out.println("La valeur ASCII de "+ch+" est: " + ascii);
+    }
+
+}
