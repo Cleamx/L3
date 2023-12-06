f01f4e6863f7b25a213dac9d92e55c4ff2d280ec
clem <clementine.mirande@gmail.com>
2023-12-06 09:08:38 +0100

    ""

--- UEINF1/TD6/ex1/Main.java
+++ UEINF1/TD6/ex1/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Personne p = new Personne("Doe", "John", 42);
         Serialiser s = new Serialiser();
-        s.enregistrerPersonne(p, "ex1/personne.txt");
-        
+        s.enregistrerPersonne(p, "personne.txt");
         Deserialiser d = new Deserialiser();
         Personne p2 = d.lirePersonne("personne.txt");
         System.out.println(p2);
