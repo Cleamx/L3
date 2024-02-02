df4fd77289841179d78eda7a36615d2d6bdf2992
clem <clementine.mirande@gmail.com>
2024-02-02 08:52:39 +0100

    "Qualité logicielle"

---
+++ S2/quali_logi/ex1.py
@@ -0,0 +1,15 @@
+x = 1
+y = 0
+z = 1
+
+
+if (x == 1 ):
+    if (y == 0):
+        if (z == 1):
+            print("cas 1")
+        else:
+            print("cas 2")
+    else:
+        print("cas 3")
+else:
+    print("cas 3")
